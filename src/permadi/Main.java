fd2ff628e07aefa34a4b86ce8ce695f28ec62a22
permadiekapermana <permadiekapermana@gmail.com>
2021-02-01 13:05:30 +0700

    project eclipse

---
+++ src/permadi/Main.java
@@ -0,0 +1,34 @@
+package permadi;
+
+import java.util.Scanner;
+
+public class Main {
+	
+public static void main(String[] args) {
+		
+	//	object spiral dengan tipe Pola
+	Pola spiral = new Spiral();
+	System.out.print("Masukkan Kalimat\t: ");
+	//	scanner input user
+    Scanner kalimat = new Scanner(System.in);
+    //	get input user
+    String input = kalimat.nextLine();
+
+    // validasi length input
+    if(input.length()>250){       
+      System.out.println("Panjang Kalimat yang diinputkan tidak boleh lebih dari 250 karakter!");
+    }else if (input.length()<=250){ 
+    
+    //	spiral.displaySpiral(input);
+    String encrypted = spiral.enkripsiKalimatSpiral(input);
+    System.out.println("\nEnkripsi Kalimat\t: " + encrypted);
+    //  spiral.displaySpiralDeskripsi(encrypted);
+    String descrypted = spiral.deskripsiKalimatSpiral(encrypted);
+    System.out.println("\nDesripsi Kalimat\t: " + descrypted);
+
+    // end if
+    }
+        
+	}
+
+}
---
+++ src/permadi/Pola.java
@@ -0,0 +1,14 @@
+package permadi;
+
+public abstract class Pola {
+	
+	public void displaySpiral(String input) {
+		
+	}
+	public void displaySpiralDeskripsi(String encrypted) {
+		
+	}
+	public abstract String enkripsiKalimatSpiral(String input);
+	public abstract String deskripsiKalimatSpiral(String encrypted);
+	
+}
